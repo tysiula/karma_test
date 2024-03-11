889233885c3affdf3c6917754880f8c4a3708a89
tysiula <136995321+tysiula@users.noreply.github.com>
2024-03-11 14:07:20 +0100

    correct test.js

--- input.test.js
+++ input.test.js
@@ -3,21 +3,21 @@ describe('String Calculator', function() {
     beforeEach(function() {
       var fixture = `<div id="fixture">
 
-         <span id="nameResult"></span>
-             <label class="a">name<input id="name" type="text"></label>
-         <span id="LastNameResult"></span>
-             <label class="a">last name<input id="lastName" type="text"></label>
-         <span id="birthdayResult"></span>
-             <label class="a">birthday (Age 18+)<input id="birthday" type="date"></label>
-         <span id="emailResult"></span>
-             <label class="a">email<input id="email" type="text"></label>
+          <span id="nameResult"></span>
+                 <label class="a">Users name<input id="name" type="text"></label>
+             <span id="lastNameResult"></span>
+                 <label class="a">Users last name<input id="lastName" type="text"></label>
+             <span id="birthdayResult"></span>
+                 <label class="a">Users birthday (Age 18+)<input id="birthday" type="date"></label>
+             <span id="emailResult"></span>
+                 <label class="a">Users email<input id="email" type="text"></label>
              <input id="add-btn" type="button" value="Add">
     </div>`;
       document.body.insertAdjacentHTML(
         'afterbegin',
         fixture);
     });
-    
+
 
     afterEach(function() {
       document.body.removeChild(document.getElementById('fixture'));
